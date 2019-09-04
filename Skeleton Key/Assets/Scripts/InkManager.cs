a66285edfd80d7bfd0e6ba3b879611a1cc88f3b6
ElizabethBallou <epb267@nyu.edu>
2019-09-04 12:09:35 -0400

    Working to integrate Ink choices and UI

--- Skeleton Key/Assets/Scripts/InkManager.cs
+++ Skeleton Key/Assets/Scripts/InkManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
 using UnityEngine;
@@ -10,128 +11,207 @@ public class InkManager : MonoBehaviour
 	[SerializeField] private TextAsset inkJSONAsset;
 	private Story story;
 
-	public Canvas canvas;
+	//Parser
+	private StoryParser storyParser;
+
+
+	[Header("Story Information")] [SerializeField]
+	public StoryState CurrentStoryState;
+
+	private bool textDone = false;
+
 	public CharacterManager _characterManager;
 	public GameManager _gameManager;
 
-	// UI Prefabs
-	private Image textboxPrefab;
-	private Text textPrefab;
-	private Button buttonPrefab;
+	// UI stuff
+	private Canvas uiCanvas;
+	private Image dialogueBox;
+	private Text dialogue;
+	private string currentText;
+	private Button choicebutton1;
+	private Button choicebutton2;
+	private Button choicebutton3;
 
-	//private Image textBox;
-	//private Text storyText;
-	
+	[Header("Text Speed")] public float TextSpeed = 0.1f;
+
+	[Header("Character Information")] [SerializeField]
+	private string currentCharName;
+
+	public Character[] CharacterData;
+
+	private Dictionary<string, Character> characters;
 
 	void Start()
 	{
-		//textboxPrefab = Resources.Load<Image>("Prefabs/Ink Text Box");
-		//textBox = (Image) GameObject.Instantiate(textboxPrefab);
-		textPrefab = Resources.Load<Text>("Prefabs/Ink Text");
-		//storyText = (Text) GameObject.Instantiate(textPrefab);
-		buttonPrefab = Resources.Load<Button>("Prefabs/Ink Continue Button");
-		//textPrefab.rectTransform.SetParent(textboxPrefab.rectTransform, true);
-		//textboxPrefab.enabled = false;
+		//Find all the UI components
+		uiCanvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+		Debug.Log("Found the canvas!");
+		dialogueBox = GameObject.FindWithTag("DialogueBox").GetComponent<Image>();
+		Debug.Log("Found the dialogue box!");
+		dialogue = dialogueBox.transform.GetChild(0).GetComponent<Text>();
+		Debug.Log("Found the dialogue text!");
+		choicebutton1 = GameObject.FindWithTag("ChoiceButton1").GetComponent<Button>();
+		choicebutton2 = GameObject.FindWithTag("ChoiceButton2").GetComponent<Button>();
+		choicebutton3 = GameObject.FindWithTag("ChoiceButton3").GetComponent<Button>();
+		Debug.Log("Found the buttons!");
 		_characterManager = GetComponent<CharacterManager>();
 		_gameManager = GetComponent<GameManager>();
-		// Remove the default message
-		StartStory();
-	}
 
-	// Creates a new Story object with the compiled story which we can then play!
-	void StartStory()
-	{
-		//textboxPrefab.enabled = true;
-		story = new Story(inkJSONAsset.text);
-		story.BindExternalFunction("place_actor",
-			(string leftName, string rightName) => { _characterManager.PlaceActor(leftName, rightName); });
-		story.BindExternalFunction("change_emotion",
-			(string emotion, int ID) => { _characterManager.ChangeActorEmotion(emotion, ID); });
-
-		RefreshView();
-	}
-
-	// This is the main function called every time the story changes. It does a few things:
-	// Destroys all the old content and choices.
-	// Continues over all the lines of text, then displays all the choices. If there are no choices, the story is finished!
-	void RefreshView()
-	{
-		// Remove all the UI on screen
-		RemoveChildren();
-
-		// Read all the content until we can't continue any more
-		while (story.canContinue)
+		characters = new Dictionary<string, Character>();
+		foreach (Character character in CharacterData)
 		{
-			// Continue gets the next line of the story
-			string text = story.Continue();
-			// This removes any white space from the text.
-			text = text.Trim();
-			// Display the text on screen!
-			CreateContentView(text);
+			characters.Add(character.Name, character);
 		}
 
-		// Display all the choices, if there are any!
-		if (story.currentChoices.Count > 0)
+		CurrentStoryState = StoryState.EpisodeStart;
+
+		choicebutton1.gameObject.SetActive(false);
+		choicebutton2.gameObject.SetActive(false);
+		choicebutton3.gameObject.SetActive(false);
+
+	}
+
+	private void Update()
+	{
+		switch (CurrentStoryState)
 		{
-			for (int i = 0; i < story.currentChoices.Count; i++)
+			case StoryState.EpisodeStart:
+				EpisodeStart();
+				break;
+			case StoryState.Printing:
+				PrintingStoryUpdate();
+				break;
+			case StoryState.Reading:
+				ReadingStoryUpdate();
+				break;
+			case StoryState.EpisodeEnd:
+				EpisodeEnd();
+				break;
+		}
+	}
+
+	private void PrintingStoryUpdate()
+	{
+		if (storyParser.CanContinue())
+		{
+			currentText = storyParser.NextLine();
+			Debug.Log(currentText);
+			/*
+	
+				string lastCharName = currentCharName;
+				currentCharName = storyParser.GetVar<string>("name");
+	
+				CurrentStoryState = StoryState.Reading;
+	
+				if (currentCharName == "player" || currentCharName == "")
+				{
+					textDone = false;
+					PrintStory(Color.yellow);
+				}
+				else if (currentCharName != lastCharName && lastCharName != "player")
+				{
+					textDone = false;
+					//load new char sprite
+					//StartCoroutine(LoadCharacter());
+				}
+				else
+				{
+					textDone = false;
+					PrintStory(characters[currentCharName].TextColor);
+				}
+				*/
+			PrintStory(Color.black);
+		}
+
+		//Do we have a choice
+		else if (storyParser.HasChoice())
+		{
+
+			if (story.currentChoices.Count > 0)
 			{
-				Choice choice = story.currentChoices[i];
-				Button button = CreateChoiceView(choice.text.Trim());
-				// Tell the button what to do when we press it
-				button.onClick.AddListener(delegate { OnClickChoiceButton(choice); });
-			}
-		}
-		// If we've read all the content and there's no choices, the story is finished!
-		else
-		{
-			Button choice = CreateChoiceView("End of story.\nRestart?");
-			choice.onClick.AddListener(delegate { StartStory(); });
-		}
-	}
+				for (int i = 0; i < story.currentChoices.Count; i++)
+				{
+					Choice choice = story.currentChoices[i];
+					Button button = storyParser.CreateChoiceView(choice.text.Trim());
+					// Tell the button what to do when we press it
+					button.onClick.AddListener(delegate { OnClickChoiceButton(choice); });
 
+				}
+			}
+
+			//The conversation is over
+			else
+				{
+					CurrentStoryState = StoryState.EpisodeEnd;
+					//the dialogue box vanishes
+					dialogueBox.enabled = false;
+				}
+			}
+		
+	}
+	
 	// When we click the choice button, tell the story to choose that choice!
-	void OnClickChoiceButton(Choice choice)
-	{
-		story.ChooseChoiceIndex(choice.index);
-		RefreshView();
+	void OnClickChoiceButton (Choice choice) {
+		story.ChooseChoiceIndex (choice.index);
 	}
 
-	// Creates a button showing the choice text
-	void CreateContentView(string text)
-	{
-		Text storyText = Instantiate(textPrefab) as Text;
-		storyText.text = text;
-		storyText.transform.SetParent(canvas.transform);
-	}
-
-	// Creates a button showing the choice text
-	Button CreateChoiceView(string text)
-	{
-		// Creates the button from a prefab
-		Button choice = Instantiate(buttonPrefab) as Button;
-		choice.transform.SetParent(canvas.transform);
-
-		// Gets the text from the button prefab
-		Text choiceText = choice.GetComponentInChildren<Text>();
-		choiceText.text = text;
-
-		// Make the button expand to fit the text
-		HorizontalLayoutGroup layoutGroup = choice.GetComponent<HorizontalLayoutGroup>();
-		layoutGroup.childForceExpandHeight = false;
-
-		return choice;
-	}
-
-	// Destroys all the children of this gameobject (all the UI)
-	void RemoveChildren()
-	{
-		for (int i = 0; i < canvas.transform.childCount; i++)
+	private void PrintStory(Color textColor)
 		{
-			if (canvas.transform.GetChild(i).CompareTag("destroyable"))
+			StartCoroutine(PrintText(textColor));
+		}
+
+		//Update while the player is reading
+		private void ReadingStoryUpdate()
+		{
+			if (textDone && (Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButtonDown(0)))
 			{
-				Destroy(gameObject);
+				CurrentStoryState = StoryState.Printing;
 			}
+		}
+
+		private void EpisodeStart()
+		{
+			TextAsset storyFile = Resources.Load<TextAsset>("Skeleton Key inky file");
+			Debug.Log("Story file loaded");
+			storyParser = new StoryParser(storyFile);
+			CurrentStoryState = StoryState.Printing;
+		}
+
+		private void EpisodeEnd()
+		{
+			//chapter++;
+			dialogue.text = "";
+			currentCharName = "";
+		}
+
+		//Coroutine to print text letter-by-letter
+		private IEnumerator PrintText(Color textColor)
+		{
+			for (int i = 0; i < currentText.Length; i++)
+			{
+				string subStr = currentText.Substring(0, i);
+				dialogue.text = subStr;
+				dialogue.color = textColor;
+				yield return new WaitForSeconds(TextSpeed);
+			}
+
+			textDone = true;
 		}
 	}
 
-}
+	public enum StoryState
+	{
+		EpisodeStart,
+		Printing,
+		Reading,
+		EpisodeEnd
+	}
+
+	[Serializable]
+	public struct Character
+	{
+		public string Name;
+		public Sprite Sprite;
+		public Color TextColor;
+	}
+		
---
+++ Skeleton Key/Assets/Scripts/StoryParser.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.Runtime;
+using UnityEngine.UI;
+
+public class StoryParser
+{
+    private Story story;
+
+    private TextAsset myText;
+
+    private Canvas myCanvas;
+
+    private Button myButton;
+    public StoryParser(TextAsset text)
+    {
+        myText = text;
+        LoadStory();
+    }
+    
+    private void LoadStory()
+    {
+        story = new Story(myText.text);
+        foreach (string variable in story.variablesState)
+        {
+            Debug.Log(story.variablesState[variable]);
+        }
+        Debug.Log("story loaded");
+    }
+
+    //Gets next line of the story
+    public string NextLine()
+    {
+        if(story.canContinue)
+            return story.Continue();
+        return "";
+    }
+
+    //Can the story continue
+    public bool CanContinue()
+    {
+        return story.canContinue;
+    }
+    
+    //Do we have a choice to make here
+    public bool HasChoice()
+    {
+        return story.currentChoices.Count > 0;
+    }
+
+    //Get a variable
+    public T GetVar<T>(string varName)
+    {
+        T variable = default(T);
+        foreach (string var in story.variablesState)
+        {
+            if(varName == var)
+                variable = (T)story.variablesState[var];
+        }
+
+        return (T)variable;
+    }
+    
+    public void LoadNewStory(TextAsset newtext)
+    {
+        myText = newtext;
+        LoadStory();
+    }
+
+    public void MakeChoice(int index)
+    {
+        if(index <= story.currentChoices.Count-1)
+            story.ChooseChoiceIndex(index);
+    }
+    
+    public void ChangeVariable(string varName, string newData)
+    {
+        foreach (string variable in story.variablesState)
+        {
+            if (variable == varName)
+            {
+                story.variablesState[variable] = newData;
+            }  
+        }
+    }
+    
+    // Creates a button showing the choice text. Taken from default Ink code
+    public Button CreateChoiceView (string text) {
+        // Creates the button from a prefab
+        Button choice = myButton;
+        choice.transform.SetParent (myCanvas.transform, false);
+		
+        // Gets the text from the button prefab
+        Text choiceText = choice.GetComponentInChildren<Text> ();
+        choiceText.text = text;
+
+        // Make the button expand to fit the text
+        HorizontalLayoutGroup layoutGroup = choice.GetComponent <HorizontalLayoutGroup> ();
+        layoutGroup.childForceExpandHeight = false;
+
+        return choice;
+    }
+    
+}
