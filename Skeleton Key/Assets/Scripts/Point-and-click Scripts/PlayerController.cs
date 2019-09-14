518b2edf9febc1306f566e7c5aedef2fb91bbe68
connorcarson <cc3237@nyu.edu>
2019-09-13 21:58:24 -0400

    Added placeholder cursor textures and functional alternating cursors. Examine, talk, and print are currently just printing to the console.

--- Skeleton Key/Assets/Scripts/Point-and-click Scripts/PlayerController.cs
+++ Skeleton Key/Assets/Scripts/Point-and-click Scripts/PlayerController.cs
@@ -6,33 +6,76 @@ public class PlayerController : MonoBehaviour
 {
     //private GameObject rb2d;
     private Camera cam;
-    private Vector2 playerPos;
-    private Vector2 currentPos;
-    public int speed = 1;
-    private Vector2 newPos;
-    private bool isMoving;
+    private Texture2D[] cursors;
     private SpriteRenderer playerSprite;
+    private Vector2 newPos;
+    private int cursorIndex;
+    
+    public Texture2D walkCursor;
+    public Texture2D examineCursor;
+    public Texture2D talkCursor;
+    public Texture2D takeCursor;
+    
+    public int speed = 1;
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        //rb2d = GetComponent<Rigidbody2D>();
+        cursors = new[] {walkCursor, examineCursor, talkCursor, takeCursor};
+        
         cam = Camera.main;
-        playerPos = transform.position;
-        currentPos = playerPos;
+        Cursor.SetCursor(cursors[0], Vector2.zero, CursorMode.Auto);
+        cursorIndex = 0;
         playerSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonUp(1))
+        {
+            ChangeCursor();
+        }
+        
         if (Input.GetMouseButtonUp(0))
         {
-            SetTargetPosition();
+            switch (cursorIndex)
+            {
+                case 0:
+                    SetTargetPosition();
+                    break;
+                case 1:
+                    Debug.Log("You examine the object.");
+                    break;
+                case 2:
+                    Debug.Log("It doesn't say much.");
+                    break;
+                case 3:
+                    Debug.Log("It doesn't budge.");
+                    break;
+                default:
+                    break;
+            }
         }
+        
         MovePlayer();
     }
 
+    void ChangeCursor()
+    {
+        if (cursorIndex < cursors.Length - 1)
+        {
+            Cursor.SetCursor(cursors[cursorIndex + 1], Vector2.zero, CursorMode.Auto);
+            cursorIndex += 1;
+        }
+        else if (cursorIndex == cursors.Length - 1)
+        {
+            Cursor.SetCursor(cursors[0], Vector2.zero, CursorMode.Auto);
+            cursorIndex = 0;
+        }
+    }
+    
     void SetTargetPosition()
     {
         newPos = cam.ScreenToWorldPoint(Input.mousePosition);
