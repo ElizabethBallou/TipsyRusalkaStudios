dc08409f589699abb1aed6a3c3fed10108143ac0
connorcarson <cc3237@nyu.edu>
2019-09-13 20:45:23 -0400

    Added placeholder player character sprite with basic walking functionality.

---
+++ Skeleton Key/Assets/Scripts/Point-and-click Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //private GameObject rb2d;
+    private Camera cam;
+    private Vector2 playerPos;
+    private Vector2 currentPos;
+    public int speed = 1;
+    private Vector2 newPos;
+    private bool isMoving;
+    private SpriteRenderer playerSprite;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb2d = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+        playerPos = transform.position;
+        currentPos = playerPos;
+        playerSprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            SetTargetPosition();
+        }
+        MovePlayer();
+    }
+
+    void SetTargetPosition()
+    {
+        newPos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void MovePlayer()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, newPos, Time.deltaTime * speed);
+        if(newPos.x < transform.position.x)
+        {
+            playerSprite.flipX = true;
+        }
+        if(newPos.x > transform.position.x)
+        {
+            playerSprite.flipX = false;
+        }
+    }
+}
