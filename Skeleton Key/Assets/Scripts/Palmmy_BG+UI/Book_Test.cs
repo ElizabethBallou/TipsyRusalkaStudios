23ebe7d6db556555123c1b85d1c9c2536343e7fe
palmmyvillage <43654403+palmmyvillage@users.noreply.github.com>
2019-12-03 12:13:37 -0500

    Create Palmmy_Branch and Apply PlaceHolder Art Assets For testing

---
+++ Skeleton Key/Assets/Scripts/Palmmy_BG+UI/Book_Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Book_Test : MonoBehaviour
+{
+    private SpriteRenderer Book;
+    private SpriteRenderer mySpr;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpr = GetComponent<SpriteRenderer>();
+        Book = GameObject.Find("Book").GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        transform.localScale *= 1.6f;
+        mySpr.color = Color.red;
+        mySpr.sortingLayerName = "Zoomed";
+        Book.color = Color.white;
+    }
+
+    private void OnMouseExit()
+    {
+        transform.localScale /= 1.6f;
+        mySpr.color = Color.white;
+        mySpr.sortingLayerName = "Default";
+        Book.color = new Color(1,1,1,0);
+    }
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("ZoomedBook");
+    }
+}
---
+++ Skeleton Key/Assets/Scripts/Palmmy_BG+UI/Shelves_Test.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Shelves_Test : MonoBehaviour
+{
+    public SpriteRenderer BG;
+    private SpriteRenderer mySpr;
+    private bool isHover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        mySpr.color = new Color(1,0.2f,0.2f,0.5f);
+        isHover = true;
+        //BG.color = new Color(1,1,1,0.8f);
+    }
+
+    private void OnMouseExit()
+    {
+        mySpr.color = new Color(1,1,1,0);
+        isHover = false;
+        //BG.color = Color.white;
+    }
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("ZoomedShelf");
+    }
+}
